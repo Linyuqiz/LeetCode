1462f3c99c14cb0d7dac9e274818caeb6a7c4e12
axemc <192119741@qq.com>
2022-09-01 00:42:19 +0800

    2022-9-1

--- rust/src/leetcode/editor/cn/_0001_two_sum.rs
+++ rust/src/leetcode/editor/cn/_0001_two_sum.rs
@@ -17,12 +17,12 @@ impl Solution {
     #[allow(dead_code)]
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut hash = std::collections::HashMap::<i32, i32>::new();
-        for (i, num) in nums.iter().enumerate() {
-            let value = target - num;
+        for (i, v) in nums.iter().enumerate() {
+            let value = target - v;
             if hash.contains_key(&value) {
                 return vec![*hash.get(&value).unwrap(), i as i32];
             }
-            hash.insert(*num, i as i32);
+            hash.insert(*v, i as i32);
         }
         return vec![];
     }
