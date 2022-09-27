cdedaa9caf7269aff4f428acb5e6bf23705f60fd
axemc <192119741@qq.com>
2022-09-27 20:24:01 +0800

    2022-9-27

--- go/src/leetcode/editor/cn/0001_two_sum.go
+++ go/src/leetcode/editor/cn/0001_two_sum.go
@@ -6,16 +6,7 @@ func main() {
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func twoSum(nums []int, target int) []int {
-	hashMap := make(map[int]int, 0)
-	for i, v := range nums {
-		number := target - v
-		// map 取值返回的是两个变量
-		if j, ok := hashMap[number]; ok {
-			return []int{j, i}
-		}
-		hashMap[v] = i
-	}
-	return []int{}
+
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ go/src/leetcode/editor/cn/0206_reverse_linked_list.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	println("206、reverse_linked_list")
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
